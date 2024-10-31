3b46d40ef13cc848c22d322aa104141054949ead
vinicius-pretto <vini.prettodasilva@gmail.com>
2024-10-31 09:38:24 -0300

    layout adjustments in the card component

--- src/components/core/Card/Card.stories.tsx
+++ src/components/core/Card/Card.stories.tsx
@@ -8,21 +8,23 @@ const meta: Meta<typeof Card> = {
   component: Card,
   render: () => {
     return (
-      <Card>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Typography htmlTag="h1" size="lg">
-            Personal Information
-          </Typography>
-          <Typography htmlTag="p" size="sm">
-            Please provide your personal details so we can get to know you
-            better.
-          </Typography>
-        </Card.Body>
-        <Card.Actions>
-          <Button>Next Step</Button>
-        </Card.Actions>
-      </Card>
+      <div style={{ maxWidth: "30rem" }}>
+        <Card>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Typography htmlTag="h1" size="lg" weight="semibold">
+              Personal Information
+            </Typography>
+            <Typography htmlTag="p" size="sm">
+              Please provide your personal details so we can get to know you
+              better.
+            </Typography>
+          </Card.Body>
+          <Card.Actions>
+            <Button>Next Step</Button>
+          </Card.Actions>
+        </Card>
+      </div>
     );
   },
 } satisfies Meta<typeof Card>;
