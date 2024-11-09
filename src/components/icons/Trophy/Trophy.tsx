ebaa5f88c1de8decc59ef58a3dbebbd41bcd9857
vinicius-pretto <vini.prettodasilva@gmail.com>
2024-11-09 15:30:58 -0300

    adding icons

---
+++ src/components/icons/Trophy/Trophy.tsx
@@ -0,0 +1,63 @@
+export const Trophy = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden={true}
+      focusable={false}
+    >
+      <g clip-path="url(#clip0_1_137)">
+        <path
+          d="M6.66663 17.5H13.3333"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M10 14.1667V17.5"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M5.83337 3.33333H14.1667"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M14.1667 3.33333V9.99999C14.1667 11.1051 13.7277 12.1649 12.9463 12.9463C12.1649 13.7277 11.1051 14.1667 10 14.1667C8.89497 14.1667 7.83516 13.7277 7.05376 12.9463C6.27236 12.1649 5.83337 11.1051 5.83337 9.99999V3.33333"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M2.5 7.49999C2.5 7.94202 2.67559 8.36595 2.98816 8.67851C3.30072 8.99107 3.72464 9.16666 4.16667 9.16666C4.60869 9.16666 5.03262 8.99107 5.34518 8.67851C5.65774 8.36595 5.83333 7.94202 5.83333 7.49999C5.83333 7.05797 5.65774 6.63404 5.34518 6.32148C5.03262 6.00892 4.60869 5.83333 4.16667 5.83333C3.72464 5.83333 3.30072 6.00892 2.98816 6.32148C2.67559 6.63404 2.5 7.05797 2.5 7.49999Z"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M14.1666 7.49999C14.1666 7.94202 14.3422 8.36595 14.6548 8.67851C14.9673 8.99107 15.3913 9.16666 15.8333 9.16666C16.2753 9.16666 16.6992 8.99107 17.0118 8.67851C17.3244 8.36595 17.5 7.94202 17.5 7.49999C17.5 7.05797 17.3244 6.63404 17.0118 6.32148C16.6992 6.00892 16.2753 5.83333 15.8333 5.83333C15.3913 5.83333 14.9673 6.00892 14.6548 6.32148C14.3422 6.63404 14.1666 7.05797 14.1666 7.49999Z"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_137">
+          <rect width="20" height="20" fill="currentColor" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
