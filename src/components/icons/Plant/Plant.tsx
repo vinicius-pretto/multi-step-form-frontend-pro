63658d879f2087c33b70eb54469b8d15f7f7518d
vinicius-pretto <vini.prettodasilva@gmail.com>
2024-11-10 18:25:23 -0300

    fix svg attributes

--- src/components/icons/Plant/Plant.tsx
+++ src/components/icons/Plant/Plant.tsx
@@ -9,27 +9,27 @@ export const Plant = () => {
       aria-hidden={true}
       focusable={false}
     >
-      <g clip-path="url(#clip0_1_120)">
+      <g clipPath="url(#clip0_1_120)">
         <path
           d="M10 8.33333C10 7.00725 9.47322 5.73548 8.53553 4.7978C7.59785 3.86012 6.32608 3.33333 5 3.33333H2.5V5C2.5 6.32608 3.02678 7.59785 3.96447 8.53553C4.90215 9.47321 6.17392 10 7.5 10H10"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10 11.6667C10 10.3406 10.5268 9.06882 11.4645 8.13113C12.4021 7.19345 13.6739 6.66667 15 6.66667H17.5V7.5C17.5 8.82608 16.9732 10.0979 16.0355 11.0355C15.0979 11.9732 13.8261 12.5 12.5 12.5H10"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10 16.6667V8.33333"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
